64b71ef9b3956212bc02574bcb5282c7f745ce72
eijidaoxuan <altonymous0@gmail.com>
2025-04-21 22:10:35 +0700

    Modify Functions.py

--- Functions.py
+++ Functions.py
@@ -29,3 +29,26 @@ mypy ...            #check all test files in folder   (   file name: test_... .p
 #Python debugger: Debug python file (use a red dot)
 
 -h                  #help in terminal  
+
+pwd                 #Print Working Directory
+git config --list   #See git setting
+git init            #Placing main branch in working directory (.git)
+git help            #For searching commands
+git add ... .py     #Or git add .   (moving all file)   for moving file from working tree to staging area
+git commit -m "..." #For moving file from staging area to history with massage
+#If file just changed, git commit -am "..."   (add and commit in the same time)
+git status          #Status = branch name, working tree, staging area, history
+git log             #Or git log -3  (3 last log)    or git log -- ...  (log with ... massage while commiting)
+git checkout ...    #... = 5 letter from commit hash    or    git checkout ... -- ... .py   remake file that was changed
+git checkout ...    #... = name branch, changing branch to ...
+git branch          #Mentioning branch   or   git branch ...  for making branch
+git log --all --decorate --oneline --graph  #Making branch and commit graph
+alias ...="..."     #Making function that include "..."     can use it with   ...
+git merge ...       #Merging name branch (...) to branch now
+git merge --merged  #Mention the branch name that has merged
+git merge -d ...    #Delete branch (...)
+git clone ...       #...=https://github...    moving file from remote(github) to git
+git remote          #Or git remote -v   for see remote name and source
+git push            #Pushing remote to main in graph
+git config --global user.email "..."  #Changing email to "..."
+git config --global user.name "..."   #Changing username to "..."
