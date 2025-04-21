0f1ddd318b2ab328d2881e59a7ed26c3abfaadb7
Eiji Daoxuan <altonymous0@gmail.com>
2025-04-21 21:40:34 +0700

    Adding Functions.py

---
+++ Functions.py
@@ -0,0 +1,31 @@
+#Terminal Phyton
+code ... .py        #make file
+python ... .py      #run file
+ls                  #list
+cp ... .py ... .py  #copy file
+mv ... .py ...      #move file into folder
+mv ... .py ... .py  #change file name
+mv ... .py ..       #move out file from folder
+rm ... .py          #remove file (Y or N if savety check)
+clear               #clean terminal
+mkdir ...           #make folder
+rmdir ...           #remove folder
+cd .../             #go into folder
+cd ..               #go out from folder
+
+black ... .py        #black code (Formating)    Go into folder where file is first
+
+__init__.py         #init file in folder to make it a package
+
+pip list            #list of installed packages
+pip uninstall ...   #uninstall package
+
+pytest ... .py      #run test file (from folder.file / from .file / from file import function)
+pytest ...          #run all test files in folder   (   file name: test_... .py, def function: def test_...(): )
+
+mypy ... .py        #check type hinting (from folder.file / from .file / from file import function)
+mypy ...            #check all test files in folder   (   file name: test_... .py, def function: def test_...(): )
+
+#Python debugger: Debug python file (use a red dot)
+
+-h                  #help in terminal  
